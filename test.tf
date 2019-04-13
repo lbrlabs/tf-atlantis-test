009043cdae8caa065282dd3629debe0051465920
Lee Briggs <lbriggs@apptio.com>
2019-04-12 19:26:14 -0700

    Blag

--- test.tf
+++ test.tf
@@ -3,6 +3,7 @@ resource "null_resource" "example-1" {}
 resource "null_resource" "example-2" {}
 resource "null_resource" "example-3" {}
 resource "null_resource" "example-4" {}
+resource "null_resource" "example-5" {}
 
 
 

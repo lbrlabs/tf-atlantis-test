831e60f300bc77583cd5f76775a7b914b70cfca6
Lee Briggs <lbriggs@apptio.com>
2019-04-12 19:36:00 -0700

    Gah

--- test.tf
+++ test.tf
@@ -4,6 +4,7 @@ resource "null_resource" "example-2" {}
 resource "null_resource" "example-3" {}
 resource "null_resource" "example-4" {}
 resource "null_resource" "example-5" {}
+resource "null_resource" "example-6" {}
 
 
 
